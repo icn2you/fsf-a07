7be12a2cfab1d7958a21d8fc1cb281e344dc9918
Christopher Zenner <christopher.zenner@gmail.com>
2020-03-23 23:08:50 -0700

    feat: Set gameboard based on query of player ID
    - Determine if gameboard exists in RPS database. If it does not,
      create the gameboard object. If it does, grab a reference to it
      (`rps.js`).

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -23,27 +23,100 @@ const firebaseConfig = {
 class RPSGame {
   // PROPERTIES
   #db = null;
-  #icons = [];
-  #players = [
-    { name: null, wins: 0, losses: 0 },
-    { name: null, wins: 0, losses: 0 }
-  ];
+  #gameboard = null;
+  #playerID = null;
+  #opponentID = null;
   
   // METHODS
   /* *************************************************************
      Constructor Method
      - Create an object of type TriviaGame
      ************************************************************* */
-  constructor(rpsIcons, p1, p2) {
-      // this.#db = initDB(dbConfig);
+  constructor(playerID) {
+      this.#playerID = playerID;
   }
       
   /* *************************************************************
      Accessor Methods
      - Get object properties
      ************************************************************* */
-  getGameDataRef() {
+  getGameDBRef() {
     return this.#db;
+  }
+
+  getGameBoard() {
+    return this.#gameboard;
+  }
+
+  getPlayerID() {
+    return this.#playerID;
+  }
+
+  getOpponentID() {
+    return this.#opponentID;
+  }
+
+  /* *************************************************************
+     Setter Methods
+     - Set object properties.
+     ************************************************************* */   
+  setGameBoard() {
+    var gameboard = this.#db.ref(),
+        playerID = this.#playerID;
+    
+    // DEBUG:
+    console.log(`Gameboard: ${gameboard}`);
+
+    var player1 = gameboard.orderByChild('player1').equalTo(playerID),
+        player2 = null,
+        exists = false;
+
+    // Check if this player's ID is in the database.
+    player1.once('value')
+      .then(function(snapshot) {
+        var p1ChildNode = snapshot.exists();
+
+        // DEBUG:
+        console.log(`Player 1 child node exists? ${p1ChildNode}`);
+
+        // If this player's ID is found in the `player1.id` field, 
+        // set the data reference accordingly and return.
+        if (p1ChildNode) {
+          exists = true;
+          gameboard = player1;
+          return;
+        }
+        else {
+          player2 = gameboard.orderByChild('player2').equalTo(playerID);
+
+          player2.once('value')
+            .then(function(snapshot) {
+              var p2ChildNode = snapshot.exists();
+
+              // DEBUG:
+              console.log(`Player 2 child node exists? ${p2ChildNode}`);
+
+              // If this player's ID is found in the `player2.id` field,
+              // set the data reference accordingly.
+              if(p2ChildNode) {
+                gameboard = player2;
+              }
+
+              exists = p2ChildNode;
+          });
+        }
+      });
+
+    // If we didn't find the player in the database, create a new
+    // gameboard object.
+    if (!exists) {
+      gameboard.push({
+        player1ID: this.#playerID,
+        timestamp: firebase.database.ServerValue.TIMESTAMP
+      });
+    }
+
+    this.#gameboard = gameboard;
   }
 
   /* *************************************************************
@@ -59,24 +132,36 @@ class RPSGame {
 
 // Execute script once page is fully loaded.
 $(document).ready(function() {
+  // Create player ID
+  var now = new Date(),
+      id = now.getTime() + 'p' + Math.floor(Math.random() * 1000);
+  
   // Create new RPS game object.
-  let game = new RPSGame([], "", "");
+  let game = new RPSGame(id);
 
+  // Initiate the RPS game database and set the game object in the database.
   game.initDB(firebaseConfig);
+  game.setGameBoard();
 
-  var gameDB = game.getGameDataRef();
+  // Get the reference to the appropriate game object in the database.
+  var gameboard = game.getGameBoard();
 
   // DEBUG:
-  console.log("db: " + gameDB);
+  console.log("db: " + gameboard);
 
-  $('<img>').on('click', function(event) {
+  // Listen for player's choice
+  $('img').on('click', function(event) {
+    var choice = event.target.id;
 
+    // DEBUG:
+    console.log(choice);
   });
 
-  gameDB.ref().on('child_added', function(childState) {
-
+  // Listen for the most recent child.
+  gameboard.orderByChild('timestamp').limitToLast(1).on('child_added', function(snapshot) {
+    console.log(snapshot.key);
   }, function(err) {
-
+    console.log(`Error Code: ${err.code}`);
   });
 
 });
